f2612c176ca569cc91d09fdce765555d284724dc
Yatchet Laptop <theyatchet@gmail.com>
2024-03-04 22:21:47 -0500

    Start of Life do be sad

--- src/main/java/com/example/lifedobesad/SceneController.java
+++ src/main/java/com/example/lifedobesad/SceneController.java
@@ -1,24 +1,42 @@
 package com.example.lifedobesad;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import java.io.File;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
-import java.net.URI;
+
 import java.nio.file.Paths;
 
 import static java.nio.file.Paths.get;
 
-public class SceneController {
+public class SceneController extends StartAnimation{
 
-    String song = "Audio/song1.mp3";
+    private static MediaPlayer mediaPlayer;
+    private String song;
+
     @FXML
-    protected void onStartButtonClick() {
+    protected void startArc1() {
+        song = "Audio/song1.mp3";
         String mediaPath = Paths.get(song).toUri().toString();
         Media regretOST = new Media(mediaPath);
-        MediaPlayer mediaPlayer = new MediaPlayer(regretOST);
+        mediaPlayer = new MediaPlayer(regretOST);
         mediaPlayer.setVolume(0.25);
-        mediaPlayer.play();
+        System.out.println(songPlaying);
+        if(!songPlaying) {
+            songPlaying = true;
+            mediaPlayer.play();
+        }
+    }
+
+    @FXML
+    protected void startArc2() {
+        song = "Audio/Gone Forever.mp3";
+        String mediaPath = Paths.get(song).toUri().toString();
+        Media regretOST = new Media(mediaPath);
+        mediaPlayer = new MediaPlayer(regretOST);
+        mediaPlayer.setVolume(0.25);
+        if(!songPlaying) {
+            songPlaying = true;
+            mediaPlayer.play();
+        }
     }
 }
--- src/main/java/com/example/lifedobesad/StartAnimation.java
+++ src/main/java/com/example/lifedobesad/StartAnimation.java
@@ -4,20 +4,38 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.nio.file.Paths;
 
 import java.io.IOException;
 
 public class StartAnimation extends Application {
+    protected static boolean songPlaying;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StartAnimation.class.getResource("start.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Life do be sad huh");
         stage.setScene(scene);
+        playBackgroundMusic();
         stage.show();
+        System.out.println(songPlaying);
     }
 
     public static void main(String[] args) {
         launch();
     }
+
+    public void playBackgroundMusic() {
+        String song = "Audio/Gone Forever.mp3";
+        String mediaPath = Paths.get(song).toUri().toString();
+        Media regretOST = new Media(mediaPath);
+        MediaPlayer mediaPlayer = new MediaPlayer(regretOST);
+        mediaPlayer.setVolume(0.25);
+        mediaPlayer.play();
+        songPlaying = true;
+    }
+
 }
